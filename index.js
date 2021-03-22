c46c496319505516263511d83fec8da2321d82c3
charliedodds <charlesgdodds@hotmail.co.uk>
2021-03-22 21:50:19 +0000

    Fix index route

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const fs = require('fs');
 
 http
 	.createServer((req, res) => {
-		const q = url.parse(req.url);
-		const filename = '.' + q.pathname;
-		fs.readFile(filename, (err, data) => {
-			if (err) {
-				res.writeHead(404, { 'Content-Type': 'text/html' });
-				return res.end('404: Not Found');
-			}
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.write(data);
-			return res.end();
-		});
+		if (req.url === '/') {
+			res.write('index.html', (err, data) => {
+				if (err) {
+					res.writeHead(404, { 'Content-Type': 'text/html' });
+					return res.end('404: Not Found');
+				}
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				return res.end();
+			});
+		} else if (req.url === '/about' || req.url === '/contact-me') {
+			const filename = '.' + q.pathname;
+			fs.readFile(filename, (err, data) => {
+				if (err) {
+					res.writeHead(404, { 'Content-Type': 'text/html' });
+					return res.end('404: Not Found');
+				}
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write(data);
+				return res.end();
+			});
+		}
 	})
 	.listen(8080);
